1101abba5ae097075192f5793af3e145261547d1
Edgar Talledos <onimenotsuki@gmail.com>
2018-06-11 12:44:15 -0500

    Tipos de datos

--- example.js
+++ example.js
@@ -1,2 +1,18 @@
-console.log('hola mundo');
-alert('hola mundo');
+// Tipos de datos
+let number = 1;
+let stringSimple = 'Lorem ipsum';
+let stringDoble = "Lorem ipsum";
+let stringSimpleInvertido = `Lorem ipsum`;
+let arreglo = [1, 2, 3, 4, 5];
+let objeto = { arreglo,  clave: 'valor',  claveDos: 2 };
+let indefinido = undefined;
+let vacio = null;
+
+const constante = 5;
+
+console.log('el dato tipo número es: ' + number);
+console.log('el dato tipo string es: ' + stringSimple);
+console.log('el dato arreglo es: ' + arreglo);
+console.log('el dato objeto es: ' + objeto);
+console.log('el dato indefinido es: ' + indefinido);
+console.log('el dato vacio es: ' + vacio);
